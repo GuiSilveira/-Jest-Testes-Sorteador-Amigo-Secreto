e1f19ccf2a25bd3f3989d65b2dd389947322657d
Guilherme Silveira <guisilveira.cout@gmail.com>
2022-09-20 20:45:53 -0300

    Criando testes de formulário

---
+++ src/components/Formulario.tsx
@@ -0,0 +1,31 @@
+import { useRef, useState } from "react";
+import { useAdicionarParticipante } from "../state/hooks/useAdicionarParticipante";
+
+const Formulario = () => {
+  const [nome, setNome] = useState("");
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  const adicionarNaLista = useAdicionarParticipante();
+
+  const adicionarParticipante = (evento: React.FormEvent<HTMLFormElement>) => {
+    evento.preventDefault();
+    adicionarNaLista(nome);
+    setNome("");
+    inputRef.current?.focus();
+  };
+
+  return (
+    <form onSubmit={adicionarParticipante}>
+      <input
+        type="text"
+        placeholder="Insira os nomes dos participantes"
+        value={nome}
+        onChange={(evento) => setNome(evento.target.value)}
+        ref={inputRef}
+      />
+      <button disabled={!nome}>Adicionar</button>
+    </form>
+  );
+};
+
+export default Formulario;
